f9f89017ecb64d0728913a3f34918f7dec14800d
zach <zacharyjwild@gmail.com>
2018-11-20 10:04:03 -0500

    Better version of four-sum

--- algorithms/leetcode/four-sum/four-sum.py
+++ algorithms/leetcode/four-sum/four-sum.py
@@ -7,11 +7,14 @@ class Solution(object):
         """
         # num -> True / False
         num_added = {} # maybe???
-        # sum -> [index1, index2, index3]
         three_sums_map = {}
 
         #
         four_sums = []
+
+        # all unique three sums
+        # sum -> [num1, num2, num3]
+        three_sums = {}
 
         # create all unique combinations of 3
         # store there sums as the key
@@ -20,29 +23,28 @@ class Solution(object):
         for i in range(0, len(nums)):
             for x in range(i+1, len(nums)):
                 for y in range(x+1, len(nums)):
-                    three_sum = nums[i] + nums[x] + nums[y]
-                    if three_sum in three_sums_map:
-                        sum_group = three_sums_map[three_sum]
-                        sum_group.append([i, x, y])
-                        three_sums_map[three_sum] = sum_group
-                    else:
-                        three_sums_map[three_sum] = [[i, x, y]]
+                    if x != y & y != i & x != i:
+                        three_sum = nums[i] + nums[x] + nums[y]
+                        if three_sum in three_sums:
+                            three_sums[three_sum].append([nums[i],
+                                                          nums[x], nums[y]])
+                        else:
+                            three_sums[three_sum] = [[nums[i], nums[x],
+                                                     nums[y]]]
 
         for i in nums:
             complement = target - i
-            if complement in three_sums_map:
-                three_sums = three_sums_map[complement]
-                for sums in three_sums:
-                    if sums != None:
-                        three_sum_nums = [nums[sums[0]], nums[sums[1]], nums[sums[2]]]
-                        sum_group.append(three_sum_nums.append(i))
-
-        return sum_group
-
-
+            if complement in three_sums:
+                sums = three_sums[complement]
+                sum_group = []
+                for sum in sums:
+                    sum.append(i)
+                    sum_group.append(sum)
+                return sum_group
 
 solution = Solution()
-nums = [1,2,3,4,5,6,7,1]
-target = 14
+nums = [5,1,1,1,1,1,1]
+target = 8
+
 
 print(solution.fourSum(nums, target))
